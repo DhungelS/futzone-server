6e996902873ddf4ac06b947eb80a3bcda2235791
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-03-19 11:34:09 -0400

    deploy

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -43,4 +43,21 @@ router.post('/api/reviews', (req, res, next) => {
     .catch(next);
 });
 
+router.delete('/api/reviews/:id', (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).send({ error: 'You must sign in first.' });
+  }
+  const { id } = req.params;
+  Review.findByIdAndRemove(id)
+    .then(count => {
+      if (count) {
+        res.status(204).end();
+      } else {
+        next();
+      }
+    })
+    .catch(next);
+});
+
+
 module.exports = router;
