11be7579b8440f259325a4f68bd10d49c4d55c3c
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-03-19 11:56:35 -0400

    deploy

--- deploy-client/src/index.js
+++ deploy-client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+
 
 import './index.css';
 import App from './components/App';
