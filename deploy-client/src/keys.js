0455bb1535af721d0bee8ad81754335a1d8b75e5
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-03-19 16:28:59 -0400

    deploy

--- deploy-client/src/keys.js
+++ deploy-client/src/keys.js
@@ -1,4 +1,3 @@
 
-export const YOUTUBE_API_KEY =  proccess.env.YOUTUBE_API_KEY
-
-export const SOCCER_API_KEY = proccess.env.SOCCER_API_KEY
+export const YOUTUBE_API_KEY = 'AIzaSyDatNhLrI7xcUoYX9pPXHJKvzeAfYBO5cA'
+export const SOCCER_API_KEY = '515c6488f516424d97ce5b2c4090d286'
